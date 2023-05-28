0625092e9e2bfee82270a3f7bcf2053b61e35939
ouchzh <aochi_zhang@126.com>
2023-05-28 12:55:55 +0800

    标签栏

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -24,7 +24,7 @@
             <!--右侧-->
             <div class="three wide column m-mobile-hide">
               <RandomBlog :randomBlogList="randomBlogList" :class="{'m-display-none':focusMode}"/>
-<!--              <Tags :tagList="tagList" :class="{'m-display-none':focusMode}"/>-->
+              <Tags :tagList="tagList" :class="{'m-display-none':focusMode}"/>
               <!--只在文章页面显示目录-->
               <Tocbot v-if="$route.name==='blog'"/>
             </div>
